de6c5e61eee760c9aa6ef18d4fe39764a066a190
arfonfo <alfonsojose.sanchez@gmail.com>
2026-04-21 22:17:42 +0200

    feat: add CV download button in footer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { ExternalLink, FolderGit2, LUCIDE_ICONS, LucideIconProvider, MapPin } from 'lucide-angular';
 import { Menu, X, User, Mail, Phone, Github, Linkedin, Code, Briefcase, GraduationCap } from 'lucide-angular';
+import { FileText } from 'lucide-angular/src/icons';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     {
       provide: LUCIDE_ICONS,
-      useValue: new LucideIconProvider({ Menu, X, User, Mail, Phone, Github, Linkedin, Code, Briefcase, GraduationCap, FolderGit2, ExternalLink, MapPin })
+      useValue: new LucideIconProvider({ Menu, X, User, Mail, Phone, Github, Linkedin, Code, Briefcase, GraduationCap, FolderGit2, ExternalLink, MapPin, FileText })
     }
   ]
 };
